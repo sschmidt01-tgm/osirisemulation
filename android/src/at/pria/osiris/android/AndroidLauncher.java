b2978b4094946b52a8dcb4ca1c7703cbb58c17d3
asongofspaceandtime <sschmidt01@student.tgm.ac.at>
2015-04-23 17:29:55 +0200

    getting a View for Android added

--- android/src/at/pria/osiris/android/AndroidLauncher.java
+++ android/src/at/pria/osiris/android/AndroidLauncher.java
@@ -1,15 +1,27 @@
 package at.pria.osiris.android;
 
 import android.os.Bundle;
+import android.view.View;
 import at.pria.osiris.OsirisSimulation;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+/**
+ * to create a View usable in Android: get an AndroidApplicationConfiguration
+ * and a Game instance (i.e. OsirisSimulation) then get the view with
+ * initializeForView(Game, config);
+ */
 public class AndroidLauncher extends AndroidApplication {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // needed
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-        initialize(new OsirisSimulation(), config);
+        OsirisSimulation osirisSimulation = new OsirisSimulation();
+        // only for libgdx Android launcher, not needed (and this whole AndroidLauncher class)
+        // if you just use the View
+        initialize(osirisSimulation, config);
+        // magic
+        View gameView = initializeForView(osirisSimulation, config);
     }
 }
